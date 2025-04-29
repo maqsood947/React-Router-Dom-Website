5c34c0b747ab9d65caec2cc32124970312bbbf25
maqsood947 <qmrmqsd420@gmail.com>
2025-04-29 15:18:05 +0500

    code adjustment

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { postData, updateData } from '../Api/PostApi'; 
+import React, { useState, useEffect } from "react";
+import { postData, updateData } from "../Api/PostApi";
 
 export const Form = ({ data, setData, updateDataApi, setUpdateDataApi }) => {
   const [addData, setAddData] = useState({
@@ -7,16 +7,11 @@ export const Form = ({ data, setData, updateDataApi, setUpdateDataApi }) => {
     body: "",
   });
 
-
-
   const handleInputChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    setAddData(prev => ({ ...prev, [name]: value }));
+    setAddData((prev) => ({ ...prev, [name]: value }));
   };
-
-
-
 
   let isEmpty = Object.keys(updateDataApi).length === 0;
 
@@ -28,14 +23,12 @@ export const Form = ({ data, setData, updateDataApi, setUpdateDataApi }) => {
       });
   }, [updateDataApi]);
 
-
-
   const addPostData = async () => {
     try {
       const res = await postData(addData);
       if (res.status === 201) {
-        setData(prev => [res.data, ...prev]); 
-        setAddData({ title: "", body: "" });  
+        setData((prev) => [res.data, ...prev]);
+        setAddData({ title: "", body: "" });
       }
     } catch (error) {
       console.log("Error adding post:", error);
@@ -46,11 +39,11 @@ export const Form = ({ data, setData, updateDataApi, setUpdateDataApi }) => {
     try {
       const res = await updateData(updateDataApi.id, addData);
       if (res.status === 200) {
-        const updatedPosts = data.map(post => 
+        const updatedPosts = data.map((post) =>
           post.id === updateDataApi.id ? res.data : post
         );
         setData(updatedPosts);
-        setAddData({ title: "", body: "" });  
+        setAddData({ title: "", body: "" });
         setUpdateDataApi({});
       }
     } catch (error) {
@@ -69,8 +62,10 @@ export const Form = ({ data, setData, updateDataApi, setUpdateDataApi }) => {
   };
 
   return (
-    <div className='h-20 bg-red-500 self-center'>
-      <form onSubmit={handleFormSubmit} className='bg-red-400 flex flex-col p-3 gap-2'>
+    <div className="h-20 bg-red-500 self-center">
+      <form
+        onSubmit={handleFormSubmit}
+        className="bg-red-400 flex flex-col p-3 gap-2">
         <div>
           <input
             type="text"
@@ -79,7 +74,7 @@ export const Form = ({ data, setData, updateDataApi, setUpdateDataApi }) => {
             placeholder="Add Title"
             value={addData.title}
             onChange={handleInputChange}
-            className='border border-lg-black'
+            className="border border-lg-black"
           />
         </div>
         <div>
@@ -94,8 +89,8 @@ export const Form = ({ data, setData, updateDataApi, setUpdateDataApi }) => {
           />
         </div>
         <button type="submit" value={isEmpty ? "Add" : "Edit"}>
-        {isEmpty ? "Add" : "Edit"}
-      </button>
+          {isEmpty ? "Add" : "Edit"}
+        </button>
       </form>
     </div>
   );
--- src/screens/Github/GitHub.jsx
+++ src/screens/Github/GitHub.jsx
@@ -1,75 +1,81 @@
-import React, { useState } from 'react'
-import { GetPost, DeletePost } from '../../Api/PostApi'
-import { useEffect } from 'react'
-import PostCard from '../../components/Cards/PostCard'
-import Form from '../../components/Form'
+import React, { useState } from "react";
+import { GetPost, DeletePost } from "../../Api/PostApi";
+import { useEffect } from "react";
+import PostCard from "../../components/Cards/PostCard";
+import Form from "../../components/Form";
 
 function GitHub() {
-const [postData, setPostData]= useState([])
-  const[loader, setLoader] = useState(true)
+  const [postData, setPostData] = useState([]);
+  const [loader, setLoader] = useState(true);
   const [updateDataApi, setUpdateDataApi] = useState({});
 
-
-const getData= async()=>{
-  try{
-  const res = await GetPost()
-  setPostData(res.data)
-  setLoader(false)
-}
-  catch(error){
-    console.log(error);
-  } 
-}
-
-useEffect(() => {
-getData()
-}, [])
-
-
-const deletePost = async (id) => {
-  try {
-    const res = await DeletePost(id);
-
-    if (res.status === 200) {
-      const updatedData = postData.filter((item) => item.id !== id); 
-      setPostData(updatedData); 
+  const getData = async () => {
+    try {
+      const res = await GetPost();
+      setPostData(res.data);
+      setLoader(false);
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    console.log("Error deleting post:", error);
-  }
-};
+  };
 
+  useEffect(() => {
+    getData();
+  }, []);
 
-const handleEdit =(item)=> setUpdateDataApi(item)
+  const deletePost = async (id) => {
+    try {
+      const res = await DeletePost(id);
 
+      if (res.status === 200) {
+        const updatedData = postData.filter((item) => item.id !== id);
+        setPostData(updatedData);
+      }
+    } catch (error) {
+      console.log("Error deleting post:", error);
+    }
+  };
 
+  const handleEdit = (item) => setUpdateDataApi(item);
 
   return (
     <>
-    <section className='flex justify-center items-center m-5' >
-    <Form
+      <section className="flex justify-center items-center m-5">
+        {/* <Form
           data={postData}
           setData={setPostData}
           updateDataApi={updateDataApi}
           setUpdateDataApi={setUpdateDataApi}
-        />    </section>
-    <section className="flex justify-center items-center m-5">
-    {loader ? (
-      <div className="spinner-border" role="status">
-        <span className="visually-hidden">Loading...</span>
-      </div>
-    ) : (
-      <div className="grid lg:grid-cols-3 sm:grid-cols-2 md:grid-cols-3 justify-center items-center gap-6">
-          {postData?.map((item) => (
-            <div key={item?.id}>
-           <PostCard postData={item} handleDelete={()=> deletePost(item.id)} handleEdit={()=>handleEdit(item)}/>
-            </div>
-          ))}
-            </div>
-          )}
-        </section>
-     
-        </>
+        /> */}
+
+
+
+        <dialog open>
+          <div className="h-20 w-20 bg-red-500">
+          hello this ia a dialogue
+          </div>
+        </dialog>
+      </section>
+      <section className="flex justify-center items-center m-5">
+        {loader ? (
+          <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </div>
+        ) : (
+          <div className="grid lg:grid-cols-3 sm:grid-cols-2 md:grid-cols-3 justify-center items-center gap-6">
+            {postData?.map((item) => (
+              <div key={item?.id}>
+                <PostCard
+                  postData={item}
+                  handleDelete={() => deletePost(item.id)}
+                  handleEdit={() => handleEdit(item)}
+                />
+              </div>
+            ))}
+          </div>
+        )}
+      </section>
+    </>
   );
 }
-export default GitHub
+export default GitHub;
